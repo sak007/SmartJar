5a10d70e164302cf3a40b99159e4b1226554898b
Ashok Kumar Selvam <aselvam@ncsu.edu>
2022-04-10 15:28:30 -0400

    fix: jar status cmd

--- code/bot/jarStatus.py
+++ code/bot/jarStatus.py
@@ -3,13 +3,11 @@ import jarHelper
 
 def run(message, bot):
     chat_id = message.chat.id
-    
+
     msg = ''
     msg += 'connectionStatus: ' + jarHelper.get('connectionStatus') + '\n'
     msg += 'lidState: '+ jarHelper.get('lidState')+ '\n'
     msg += 'jarOnScaleState'+ jarHelper.get('jarOnScaleState')+ '\n'
-    msg += 'lockState' + jarHelper('lockState')+ '\n'
-    msg += 'weight' + jarHelper.get('weight')+ '\n'
-    bot.send_message_(chat_id, msg)
-
-
+    msg += 'lockState' + jarHelper.get('lockState')+ '\n'
+    msg += 'weight' + str(jarHelper.get('weight'))+ '\n'
+    bot.send_message(chat_id, msg)
